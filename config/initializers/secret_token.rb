5808834b412a0d7679f20c97a0c77fd6dca89ed2
Nate Wise <nathanwise@gmail.com>
2011-03-02 14:45:34 -0500

    Initial check-in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeatherNearMe::Application.config.secret_token = '8982122cb6cb38c2ea471922599464e7ebe0d8998530e5d6ad1e7e6f0c5960264d9d8dcd7a4c67493cb4624863fefff15326259e991010f133d4a5c93b6b8e66'
